4657d20825b954680cc30e70129d343ca4baf2ab
“Terris“ <terris.kremer@gmail.com>
2018-03-02 15:56:00 -0700

    add production api

--- src/api-config.js
+++ src/api-config.js
@@ -4,7 +4,7 @@ const apiVersion = 'v1';
 const hostname = window && window.location && window.location.hostname;
 
 if(hostname === 'realsite.com') {
-  backendHost = 'https://api.realsite.com';
+  backendHost = 'https://doneapi.herokuapp.com';
 } else {
   backendHost = process.env.REACT_APP_BACKEND_HOST || 'http://localhost:3001';
 }
