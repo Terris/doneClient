1091075d030dbf3fb1c699f429de717c74ce9b27
“Terris“ <terris.kremer@gmail.com>
2018-03-02 16:28:22 -0700

    add deploy process

--- src/api-config.js
+++ src/api-config.js
@@ -3,7 +3,7 @@ const apiVersion = 'v1';
 
 const hostname = window && window.location && window.location.hostname;
 
-if(hostname === 'realsite.com') {
+if(hostname === 'doneapp.surge.sh') {
   backendHost = 'https://doneapi.herokuapp.com';
 } else {
   backendHost = process.env.REACT_APP_BACKEND_HOST || 'http://localhost:3001';
